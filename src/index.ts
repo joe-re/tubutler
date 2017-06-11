9d38594c9393e279542476069970c0a7aa734386
joe-re <joe.tialtngo@gmail.com>
2017-06-12 02:24:37 +0900

    ts compile

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+// src/index.ts
+
+import Vue from "vue";
+
+let v = new Vue({
+    el: "#app",
+    template: `
+    <div>
+        <div>Hello {{name}}!</div>
+        Name: <input v-model="name" type="text">
+    </div>`,
+    data: {
+        name: "World"
+    }
+});
