1e82ec53023c355932d9e52c5ec516236a815699
joe-re <joe.tialtngo@gmail.com>
2017-06-12 04:37:05 +0900

    to sfc

---
+++ src/components/Hello.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>Hello {{name}}{{exclamationMarks}}</div>
+    <button @click="decrement">-</button>
+    <button @click="increment">+</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from 'vue-class-component'
+
+@Component({
+  props: {
+    name: String,
+    initialEnthusiasm: Number
+  }
+})
+export default class Hello extends Vue {
+  initialEnthusiasm: number
+  enthusiasm = this.initialEnthusiasm
+
+  increment() {
+    this.enthusiasm++
+  }
+
+  decrement() {
+    this.enthusiasm--
+  }
+
+  get exclamationMarks(): string {
+    return Array(this.enthusiasm + 1).join('!');
+  }
+}
+</script>
