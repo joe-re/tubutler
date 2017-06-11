d6b3bf42a4630ce694d47cafa580e6f181a02eb1
joe-re <joe.tialtngo@gmail.com>
2017-06-12 03:53:32 +0900

    Hello Component

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
-// src/index.ts
-
 import Vue from "vue";
+import HelloComponent from "./components/Hello";
 
 let v = new Vue({
     el: "#app",
     template: `
     <div>
-        <div>Hello {{name}}!</div>
         Name: <input v-model="name" type="text">
-    </div>`,
-    data: {
-        name: "World"
+        <hello-component :name="name" :initialEnthusiasm="5" />
+    </div>
+    `,
+    data: { name: "World" },
+    components: {
+        HelloComponent
     }
 });
