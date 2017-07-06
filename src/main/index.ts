f1e48875fcba000c69ef62768ed6ab4a570ddc08
joe-re <joe.tialtngo@gmail.com>
2017-07-06 18:25:50 +0900

    alwaysOnTop

--- src/main/index.ts
+++ src/main/index.ts
@@ -8,7 +8,7 @@ let win: any
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 800, height: 600, alwaysOnTop: true})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
