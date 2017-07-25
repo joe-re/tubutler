45c29e9fcc3f2e49118a92029b62f349b11c12ba
joe-re <joe.tialtngo@gmail.com>
2017-07-26 07:45:01 +0900

    use url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -40,10 +40,7 @@ module.exports = {
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]?[hash]'
-        }
+        loader: 'url-loader',
       }
     ]
   },
